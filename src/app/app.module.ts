52da53a8d418dac7e715622b0282c65f36cf717a
PaulObuba <paulobuba26@gmail.com>
2022-10-17 17:46:32 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NewsComponent } from './components/news/news.component';
+import { ServicesComponent } from './components/services/services.component';
+import { RoomsComponent } from './components/rooms/rooms.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { TestimoniesComponent } from './components/testimonies/testimonies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    NewsComponent,
+    ServicesComponent,
+    RoomsComponent,
+    GalleryComponent,
+    WelcomeComponent,
+    TestimoniesComponent
   ],
   imports: [
     BrowserModule,
